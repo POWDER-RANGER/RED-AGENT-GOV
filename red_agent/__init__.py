ef121836942947106fad7587e7c35a01908c3f02
Systems Architect • AI Tooling • Civic Monitoring <137366958+POWDER-RANGER@users.noreply.github.com>
2026-04-14 04:02:38 -0500

    fix(lint): remove ERA001 commented-out code in __init__.py
    Removed commented-out public API imports and placeholder __all__.

--- red_agent/__init__.py
+++ red_agent/__init__.py
@@ -5,22 +5,3 @@ behavioral compliance architecturally through a six-directive filter system.
 """
 
 __version__ = "3.0.0"
-
-# Public API imports will be implemented as modules are created
-# from .agent import RedAgent, RedAgentConfig, TaskExecutorFn
-# from .constants import (
-#     AgentState,
-#     ArtifactClass,
-#     ClassificationLevel,
-#     FaultClass,
-#     GateSuppressionReason,
-#     OutputDecision,
-#     ReviewAction,
-# )
-# from .intelligence import IntelligenceArtifact, RecipientClassification, ThreatModelEntry
-# from .tasking import TaskingUnit, TaskResult
-# from .recovery import RecoverySignal, generate_recovery_signal
-
-__all__ = [
-    # Placeholder - will be populated as modules are implemented
-]
